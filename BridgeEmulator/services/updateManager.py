a6648722aaa61ec2fc83bc4313923529a40536a5
SuperFashi <admin@superfashi.com>
2026-03-16 19:35:31 +0900

    properly patch

--- BridgeEmulator/services/updateManager.py
+++ BridgeEmulator/services/updateManager.py
@@ -39,6 +39,7 @@ def githubCheck() -> None:
     Check for updates on GitHub for both the main diyHue repository and the UI repository.
     Update the bridge configuration based on the availability of updates.
     """
+    return
     creation_time = get_file_creation_time("HueEmulator3.py")
     publish_time = get_github_publish_time("https://api.github.com/repos/diyhue/diyhue/branches/master")
     
@@ -158,4 +159,4 @@ def startupCheck() -> None:
         bridgeConfig["config"]["swupdate2"]["install"] = False
         update_swupdate2_timestamps()
     versionCheck()
-    # githubCheck()
+    githubCheck()
